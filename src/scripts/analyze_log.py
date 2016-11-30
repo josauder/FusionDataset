cb14ccb8ecae5384fc01be1a9782cea0ffb00f0e
Jonathan Sauder <jonathan.sauder@student.hpi.de>
2016-11-30 18:03:44 +0100

    Change junit version

--- src/scripts/analyze_log.py
+++ src/scripts/analyze_log.py
@@ -18,3 +18,21 @@ with open(logpath,"r") as f:
 
 ModeResolver=np.array(ModeResolver)
 
+total = len(ModeResolver)
+
+print "TOTAL:"
+for percentage in range(100,20,-10):
+	arr = len([x for x in ModeResolver[:,0] if x<=percentage and x>percentage-10])
+	print percentage, "\t", percentage-10, "\t",arr, "\t", "/" , "\t", total, "\t", float(arr)/total
+
+print "COUNT:"
+for i in range(2,9):
+	arr = np.array([x for x in ModeResolver if x[1]==i])
+	arrLen= len(arr)
+	print i,":",arrLen,arrLen/float(total)
+	
+	
+	for percentage in range(100,20,-10):
+		
+		arr2 = len([x for x in arr[:,0] if x<=percentage and x>percentage-10])
+		print "\t", "\t",percentage, "\t", percentage-10, "\t",arr2, "\t", "/" , "\t", total, "\t", float(arr2)/arrLen
